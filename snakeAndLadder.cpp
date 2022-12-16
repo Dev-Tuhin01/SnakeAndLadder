3cb343a89e9b01e02ce5e25b3ada1cfc4b19accb
Tuhin <dev.tuhin.1@gmail.com>
2022-12-16 17:44:09 +0530

    Created Dice Function

--- snakeAndLadder.cpp
+++ snakeAndLadder.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+/*
+    *this program is basic game of chess and ladder and is a CLI based game.
+    *in this game there are 2 players * and #. and it is a turn based game.
+    *there are 5 ladders and 7 snakes in this game. 
+*/
 
-using namespace std;
+#include <iostream>
+#include <CTIME>
+#include <TIME.H>
+#include <stdlib.H>
+
+int dice(){
+    /*
+        * This is a RNG function
+        * This function will return some number between 1 to 6 randomly
+    */
+   
+    int roll;
+    srand(time(0));
+
+    roll = rand() % 6 + 1;
+
+    return roll;
+}
 
 
 
