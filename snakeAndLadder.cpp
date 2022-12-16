640a6c290a76f13ff038071259d39f44b016672a
Tuhin <dev.tuhin.1@gmail.com>
2022-12-16 18:14:36 +0530

    created variables for player1 and player2, ladders and snakes, set up inital positions

--- snakeAndLadder.cpp
+++ snakeAndLadder.cpp
@@ -9,12 +9,21 @@
 #include <TIME.H>
 #include <stdlib.H>
 
+int player1Position = 1 , player2Position = 1;
+
+int ladder[] = {10,41,73,36,63};
+int snake[] = {19,93,87,72,55,27,48};
+
+int ladderEnd[] = {15,10,21,21,25};
+int snakeEnd[] = {-14,-50,-41,-20,-25,-11,-20};
+
+
 int dice(){
     /*
         * This is a RNG function
         * This function will return some number between 1 to 6 randomly
     */
-   
+
     int roll;
     srand(time(0));
 
